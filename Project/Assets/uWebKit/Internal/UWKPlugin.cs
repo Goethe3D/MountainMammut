9ed6d46fe519e58acfa74e32ba523ff3cc709849
Goethe3D <goethe3d@gmail.com>
2015-03-10 20:37:19 +0100

    Revert "solved unity 5 bug uwebkit"
    This reverts commit 952712d1b76f2d6271c115ad07d6ae5e7894ad60.

--- Project/Assets/uWebKit/Internal/UWKPlugin.cs
+++ Project/Assets/uWebKit/Internal/UWKPlugin.cs
@@ -44,14 +44,7 @@ public class UWKPlugin
         djson["application"] = app = new Dictionary<string,object>();
 
         app["hasProLicense"] = Application.HasProLicense();
-		string dataPath = Application.dataPath;
-		#if UNITY_5_0
-			#if !UNITY_EDITOR
-			// Unity 5 bug
-			dataPath += "/Resources";                
-			#endif 
-		#endif
-		app["dataPath"] = dataPath;
+        app["dataPath"] = Application.dataPath;
         app["persistentDataPath"] = Application.persistentDataPath;
         app["temporaryCachePath"] = Application.temporaryCachePath;
         app["isEditor"] = Application.isEditor;
