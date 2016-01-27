90067cc60868a444ab5eab9f11e8b7b0f290c2ac
Goethe3D <goethe3d@gmail.com>
2016-01-27 20:24:01 +0100

    input field sync

---
+++ Project/Assets/InputFieldSync.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputFieldSync : MonoBehaviour {
+
+	InputField inputField;
+	PhotonView photonView;
+
+	// Use this for initialization
+	void Start () {
+	
+		photonView = GetComponent< PhotonView >();
+		inputField = GetComponent< InputField >();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[RPC]
+	void setInputFieldTextRPC( string text )
+	{
+		inputField.text = text;
+	}
+
+	public void setInputFieldText( string text )
+	{
+		photonView.RPC( "setInputFieldTextRPC" , PhotonTargets.All , text );
+	}
+}
