763b1918c218c32d31f924d2e9810104f71691a1
Goethe3D <goethe3d@gmail.com>
2016-12-08 20:28:45 +0100

    Alt - Q for Quit

---
+++ Project/Assets/ApplicationShortcuts.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationShortcuts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetKeyUp(KeyCode.Q) && Input.GetKey(KeyCode.LeftAlt) )
+		{
+			Application.Quit();
+		}
+	
+	}
+}
